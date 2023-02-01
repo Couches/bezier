accb561258208a9d2fa560ded2964240edc58164
Robert De Lappe <robertgdel@gmail.com>
2023-02-01 02:56:42 -0800

    Began adding curve functionality

--- bezier.js
+++ bezier.js
@@ -3,6 +3,9 @@ const createButton = document.getElementById("create-point");
 const itemCounter = document.getElementById("item-count");
 const pointList = document.getElementById("point-list");
 
+const curves = [];
+
+var numPoints = 0;
 var points = [];
 
 // Point Object
@@ -19,6 +22,14 @@ class Point {
     toString() { return `${this.x} ${this.y}` }
 }
 
+// Set Attribute List
+// Sets attributes of an element with a map of properties
+function setAttributeList(element, props)
+{
+    Object.entries(props).forEach(([key, value]) => {
+        element.setAttributeNS(null, key, value);
+    })
+}
 
 // Create List Point
 // Creates an HTML element containing information about its respective point
@@ -38,8 +49,8 @@ function createListPoint(x, y)
         id++;
     }
     if (id >= 26) return;
-    div.id = id;
 
+    div.id = id;
     div.className = "point-list-item";
 
     pointText.innerText = `point ${String.fromCharCode(parseInt(div.id) + 97)}`;
@@ -72,18 +83,49 @@ function createListPoint(x, y)
     points.push(div);
     pointList.appendChild(points[points.length - 1]);
 
+    createPointPath(x + id * 10, y + id * 10, id);
+
     updateCounter();
+}
+
+// Create a circle at x and y, then assign id
+function createPointPath(x, y, id)
+{
+    let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+
+    text.textContent = `${String.fromCharCode(parseInt(id) + 97)}`;
+
+    setAttributeList(circle, {
+        cx: x,
+        cy: y,
+        r: 6,
+        stroke: 'orange',
+        "stroke-width": '4px',
+        fill: 'transparent',
+        id: 'p' + id
+    });
+
+    setAttributeList(text, {
+        x: x + 5,
+        y: y - 5,
+        "font-size": "13px",
+        id: 't' + id
+    })
+
+    container.appendChild(circle);
+    container.appendChild(text);
 }
 
 // Update Counter
 // Changes the 
 function updateCounter()
 {
-    let count = pointList.childElementCount;
+    numPoints = pointList.childElementCount;
     let text;
-    if (count > 0 && count < 26) text = count + "/26";
-    else if (count == 0) text = "";
-    else if (count == 26) text = "MAX"
+    if (numPoints > 0 && numPoints < 26) text = numPoints + "/26";
+    else if (numPoints <= 0) text = "";
+    else if (numPoints >= 26) text = "MAX"
 
     itemCounter.innerText = text;
 }
@@ -93,13 +135,29 @@ function createPoint(x, y)
     createListPoint(x, y);
 }
 
+// Delete Point
+// Removes point from PointList element and points array
+// and point circle from the svg
 function deletePoint(parentID)
 {
-    document.getElementById(parentID).remove();
-    console.log(`deleted ${parentID}`);
+    let target = document.getElementById(parentID);
+    points.splice(points.indexOf(target), 1);
+    target.remove();
+
+    document.getElementById("p" + parentID).remove();
+    document.getElementById("t" + parentID).remove();
+
+    //console.log(`deleted ${parentID}`);
     updateCounter();
 }
 
 createButton.addEventListener("click", () => {
-    createPoint(0, 0);
+    createPoint(100, 100);
 });
+
+function generatePath()
+{
+    
+}
+
+//curves.push(document.createElementNS('http://www.w3.org/2000/svg', 'path'));
